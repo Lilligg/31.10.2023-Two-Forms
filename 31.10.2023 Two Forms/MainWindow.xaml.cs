bb3e5fade00f6e714a220786ef4474e12c3cd245
lidia <lidia@DESKTOP-2MPGDVB>
2023-11-11 00:34:42 +0300

    Добавьте файлы проекта.

---
+++ 31.10.2023 Two Forms/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _31._10._2023_Two_Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 form2 = new Window2();
+            form2.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ 31.10.2023 Two Forms/Window2.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _31._10._2023_Two_Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow form1 = new MainWindow();
+            form1.Show();
+            this.Hide();
+        }
+    }
+}
